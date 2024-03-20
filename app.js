4028dfa2a4fe32f7d36be10e7c54b6bc5b2d2743
adityaGkapse <adityakapse2503@gmail.com>
2024-03-20 14:14:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+const apiKey = "5953e408be513eb5729a36c648bc579d";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=mertric&q=";
+
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "mist.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+
+    }
+
+
+}
+
+searchbtn.addEventListener("click", () => {
+    checkWeather(searchbox.value);
+});
